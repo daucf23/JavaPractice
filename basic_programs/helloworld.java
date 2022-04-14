82020f63c33a2443323b0ddcd22be4163826b0e3
daucf23 <davidjalmeida2@gmail.com>
2022-04-13 22:33:19 -0400

    added simple hello world

---
+++ basic_programs/helloworld.java
@@ -0,0 +1,14 @@
+import com.apple.eawt.AppEvent.FullScreenEvent;
+
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+        function1();
+    }
+
+    private static void function1() {
+        System.out.println("Hello Function1!");
+    }
+
+}
